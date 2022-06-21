ca4864899844b1ac6377ba081fd8239110fd1c38
Amit-1998-a <97940048+Amit-1998-a@users.noreply.github.com>
2022-06-21 10:20:14 +0530

    21-06-22 sqrt, addition, of array

---
+++ numpy3.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr1= np.array([[3,6],[7,5]])
+arr2=np.array([[2,4],[8,4]])
+
+Sum=np.add(arr1,arr2)
+print("addition for array \n",Sum)
+
+sum1=np.sum(arr1)
+print("\n addition of element array",sum1)
+
+Sqrt=np.sqrt(arr1)
+print("\n square root of array1",Sqrt)
+
+Tran_arr=arr1.T
+print("\n Transpose of array")
+print(Tran_arr)
